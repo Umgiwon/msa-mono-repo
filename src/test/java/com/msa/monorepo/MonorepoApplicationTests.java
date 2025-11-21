b19b11e54e41270ff94a5f307a68f654caa8b9be
umgiwon <ugwzzang@gmail.com>
2025-11-21 18:06:11 +0900

    #init

---
+++ src/test/java/com/msa/monorepo/MonorepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.monorepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonorepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
