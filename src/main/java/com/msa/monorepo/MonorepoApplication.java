3e7ecdf1f8238d8f61e5d1ca2a4432dfd30d6177
umgiwon <ugwzzang@gmail.com>
2025-12-04 16:15:19 +0900

    change : root project SpringBootApplication 주석처리

--- src/main/java/com/msa/monorepo/MonorepoApplication.java
+++ src/main/java/com/msa/monorepo/MonorepoApplication.java
@@ -1,13 +1,13 @@
-package com.msa.monorepo;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MonorepoApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MonorepoApplication.class, args);
-    }
-
-}
+//package com.msa.monorepo;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class MonorepoApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(MonorepoApplication.class, args);
+//    }
+//
+//}
